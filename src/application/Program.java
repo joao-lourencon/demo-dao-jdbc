9d9c51083c8b535aba76de2e7944744d3c0d96bb
joao-lourencon <joaolouren478@gmail.com>
2024-08-06 10:16:18 -0300

    DepartmentDao and SellerDao interfaces

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,7 +8,6 @@ import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
         Department department = new Department(1, "Books");
         Seller seller = new Seller(1, "Caio", "caio@gmail.com", new Date("1998/12/22"), 3000.0, department);
---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.List;
+
+public interface SellerDao {
+    void insert(Seller seller);
+    void update(Seller seller);
+    void deleteById(Integer integer);
+    Seller findByid(Integer integer);
+    List<Seller> findAll();
+}
