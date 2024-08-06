11450f75fff74df60f30e8d69e54c69bcea2720b
joao-lourencon <joaolouren478@gmail.com>
2024-08-05 23:50:47 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,19 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
         Department department = new Department(1, "Books");
+        Seller seller = new Seller(1, "Caio", "caio@gmail.com", new Date("1998/12/22"), 3000.0, department);
 
         System.out.println(department);
+        System.out.println(seller);
     }
 }
